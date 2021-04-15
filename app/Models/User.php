87cce14545faec80c761894b8434132a46269872
Abdoul-Aziz ZOROM <abdoulazizzorom@gmail.com>
2021-04-15 11:21:08 +0000

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Role;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -49,5 +50,10 @@ class User extends Authenticatable
     public function routeNotificationForNexmo($notification)
     {
         return $this->phone_number;
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
     }
 }
